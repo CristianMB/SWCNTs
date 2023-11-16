5882bbe08717cf208d397c308a9d290a4a456f2e
Cristian Borja <cmbp98@gmail.com>
2023-11-16 18:40:51 +0100

    Plot SWCNT and Fluorescence Refactor

--- SWCNTs_MATLAB/LambdasScript.m
+++ SWCNTs_MATLAB/LambdasScript.m
@@ -14,58 +14,94 @@ InvreseD_array = [];
 RBM_array = [];
 
 
-if 1==1
-    for n = 4:20 % Adjust the range as needed
-        for m = 1:n % Adjust the range as needed
-            result = mod(n-m,3);
-            if (result==1 || result==2)
-                [l11, l22] = calculate_lambdas(n, m);
-                m_array = [m_array, m];
-                n_array = [n_array, n];
-                l11_array = [l11_array, l11]; % Save value in the array
-                l22_array = [l22_array, l22]; % Save value in the array
-                D_array = [D_array,calculateD(n,m)];
-                InvreseD_array = [InvreseD_array,1/calculateD(n,m)];
-                RBM_array = [RBM_array,calculateRBM(n,m)];
-            end
+%Calculate the parameters required to plot
+for n = 4:15 
+    for m = 1:n
+        result = mod(n-m,3);
+        if (result==1 || result==2)
+            [l11, l22] = calculate_lambdas(n, m);
+            m_array = [m_array, m];
+            n_array = [n_array, n];
+            l11_array = [l11_array, l11]; 
+            l22_array = [l22_array, l22];
+            D_array = [D_array,calculateD(n,m)];
+            InvreseD_array = [InvreseD_array,1/calculateD(n,m)];
+            RBM_array = [RBM_array,calculateRBM(n,m)];
+            tupleArray = calculateTouple(m_array,n_array);
+            stokes_shift = l11_array-l22_array;
         end
     end
-    
-    figure;
-    subplot(1, 1, 1);
-    scatter(InvreseD_array, RBM_array)
-    xlabel('Inverse of "d" (nm-1)')
-    ylabel('RMB frequency (cm-1) ')
-
-   
-    figure;
-    subplot(1, 1, 1);
-    scatter(RBM_array, l11_array); hold on
-    scatter(RBM_array, l22_array)
-    xlabel('RMB (cm-1)')
-    ylabel('WL (nm)')
-    
-    figure;
-    subplot(1, 1, 1);
-    scatter(l11_array, l22_array)
-    xlabel('Lambda_{11} (nm)')
-    ylabel('Lambda_{22} (nm)')
-
 end
+    
 
-keySet = {  'Water1',
-            'Water2', 
-            'Water3', 
-            'Water4'};
-        
-valueSet = {[327.2, 327.2],
-            [327.2, 327.2], 
-            [327.2, 327.2], 
-            [327.2, 327.2]};
-        
-M = containers.Map(keySet, valueSet);
+figure;
+scatter(InvreseD_array, RBM_array, 'MarkerFaceColor', 'g')
+title('RBM vs. 1/d');
+for i = 1:length(InvreseD_array)
+    text(InvreseD_array(i), RBM_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+xlabel('Inverse of d (nm^{-1})')
+ylabel('RMB frequency (cm^{-1})')
 
 
+figure;
+scatter(RBM_array, l11_array, 'MarkerFaceColor', 'b'); hold on
+scatter(RBM_array, l22_array, 'MarkerFaceColor', 'r')
+title('Excitation/Emission Wavelengths vs. RBM');
+for i = 1:length(RBM_array)
+    text(RBM_array(i), l11_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+for i = 1:length(RBM_array)
+    text(RBM_array(i), l22_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+xlabel('RMB frequency (cm^{-1})')
+ylabel('Wavelength (nm)')
+    
+
+figure;
+scatter(D_array, l11_array, 'MarkerFaceColor', 'b'); hold on
+scatter(D_array, l22_array, 'MarkerFaceColor', 'r')
+title('Excitation/Emission Wavelengths vs. RBM');
+for i = 1:length(D_array)
+    text(D_array(i), l11_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+for i = 1:length(D_array)
+    text(D_array(i), l22_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+xlabel('Diameter (nm)')
+ylabel('Wavelength (nm)')
+
+
+figure;
+scatter(l11_array, l22_array, 'MarkerFaceColor', 'r')
+title('Excitation vs. Emission Wavelength');
+for i = 1:length(l11_array)
+    text(l11_array(i), l22_array(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+xlabel('Emission Wavelength (nm)')
+ylabel('Excitation Wavelength (nm)')
+
+
+figure;
+scatter(D_array, stokes_shift, 'MarkerFaceColor', 'b')
+title('Stokes shift vs. Diameter');
+for i = 1:length(D_array)
+    text(D_array(i), stokes_shift(i), tupleArray(i), 'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'right');
+end
+xlabel('Diameter (nm)')
+ylabel('Stokes shift (nm)')
+
+
+function tupleArray = calculateTouple(arr1, arr2)
+    
+    % Preallocate cell array for tuple
+    tupleArray = cell(size(arr1));
+    % Convert and concatenate element-wise
+    for i = 1:numel(arr1)
+        tupleArray{i} = ['(',num2str(arr1(i)), ',', num2str(arr2(i)),')'];
+    end
+end
+    
 function RBM = calculateRBM(n,m)
     Acons = 223.5;
     Bcons = 12.5;
---
+++ SWCNTs_MATLAB/OptimizedPlotCNT.m
@@ -0,0 +1,130 @@
+% ........................... Input Data ...................................
+n = input('Enter the Chiral coordinate n>0 & integer: ');
+m = input('Enter the Chiral coordinate m>0 & integer: ');
+l = input('Enter the nanotube length in nanometer: ');
+bl = 0.144;
+
+a = 2 * bl * cos(pi / 6);
+a1 = [1; 0] * a;
+a2 = [cos(pi / 3); sin(pi / 3)] * a;
+vector = n * a1 + m * a2;
+length_vector = sqrt(vector(1, 1)^2 + vector(2, 1)^2);
+cos_t = vector(1, 1) / length_vector;
+sin_t = vector(2, 1) / length_vector;
+T = [cos_t, sin_t; -sin_t, cos_t];
+counter = 1;
+check = 1;
+y_temp = -bl / 2;
+
+% ...................... Generating graphene plane .........................
+
+graphene_plane = [];
+for j = 0:5 * round(l / bl)
+    if (check == 3)
+        xo = a / 2;
+        check = check + 1;
+        y_temp = y_temp + bl / 2;
+    elseif (check == 4)
+        x = a / 2;
+        check = 1;
+        y_temp = y_temp + bl;
+    elseif (check == 2)
+        xo = 0;
+        check = check + 1;
+        y_temp = y_temp + bl;
+    else
+        xo = 0;
+        check = check + 1;
+        y_temp = y_temp + bl / 2;
+    end
+    for i = -5 * round(l / a):5 * round(l / a)
+        x = xo + i * a;
+        y = y_temp;
+        R = [x; y];
+        R_temp = T * R;
+        if (and(R_temp(1, 1) >= 0, R_temp(1, 1) < 1.01 * length_vector))
+            if (and(R_temp(2, 1) >= 0, R_temp(2, 1) < l))
+                graphene_plane(counter, 1) = counter;
+                graphene_plane(counter, 2) = R_temp(1, 1);
+                graphene_plane(counter, 3) = R_temp(2, 1);
+                counter = counter + 1;
+            end
+        end
+    end
+end
+
+% .............. Rolling the Graphene plane ..........................
+Number_atoms = size(graphene_plane, 1);
+r = length_vector / 2 / pi;
+
+Atom_data = zeros(Number_atoms, 4);
+for i = 1:Number_atoms
+    teta = graphene_plane(i, 2) / r;
+    Atom_data(i, 1) = i;
+    Atom_data(i, 2) = r * sin(teta);
+    Atom_data(i, 3) = graphene_plane(i, 3);
+    Atom_data(i, 4) = r * cos(teta) + r;
+end
+
+% ............. Generating Bond matrix ...............................
+Bond_matrix = zeros(Number_atoms);
+
+for i = 1:Number_atoms
+    for j = i + 1:Number_atoms
+        distance = norm(Atom_data(i, 2:4) - Atom_data(j, 2:4));
+        if (distance < 1.1 * bl)
+            Bond_matrix(i, j) = 1;
+            Bond_matrix(j, i) = 1;
+        end
+    end
+end
+
+% .................. Drawing atoms and bonds .........................
+figure;
+hold on;
+% Set color for atoms
+atom_color = [0 0 0];
+
+for i = 1:Number_atoms
+    x = Atom_data(i, 2);
+    y = Atom_data(i, 3);
+    z = Atom_data(i, 4);
+    [xx, yy, zz] = sphere();
+    rr = 0.075 * a;
+    surf(rr * xx + x, rr * yy + y, rr * zz + z, 'EdgeColor', 'none', 'LineStyle', 'none', 'FaceLighting', 'phong', 'FaceColor', atom_color)
+    axis equal;
+end
+
+% Set color for bonds
+bond_color = [0.50 0.200 0.50];
+
+for i = 1:Number_atoms
+    for j = i + 1:Number_atoms
+        if (Bond_matrix(i, j) == 1)
+            x = Atom_data(i, 2);
+            y = Atom_data(i, 3);
+            z = Atom_data(i, 4);
+            xx = Atom_data(j, 2);
+            yy = Atom_data(j, 3);
+            zz = Atom_data(j, 4);
+            line([x, xx], [y, yy], [z, zz], 'Color', bond_color, 'LineWidth', 1)
+        end
+    end
+end
+
+% .................. Generating output files .........................
+Atom_data = Atom_data';
+Bond_data = find(Bond_matrix == 1);
+[i, j] = ind2sub(size(Bond_matrix), Bond_data);
+Bond_data = [i, j];
+
+fid1 = fopen('Atom_data.txt', 'wt');
+fid2 = fopen('Bond_data.txt', 'wt');
+fprintf(fid1, 'Atom_number     X(nm)     Y(nm)     Z(nm)     \n');
+fprintf(fid2, 'Bond_number      Atom1      Atom2     \n');
+fprintf(fid1, '%d  %15.10f  %15.10f  %15.10f  \n', Atom_data);
+fprintf(fid2, '%d  %d  %d  \n', Bond_data);
+fclose(fid1);
+fclose(fid2);
+
+hold off;
