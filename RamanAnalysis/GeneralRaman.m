88a3bda402c9c880b183164c5b1b74b9fe5aab49
Cristian Borja <cmbp98@gmail.com>
2024-04-04 16:28:30 +0200

    Updated Scripts 240404

--- RamanAnalysis/GeneralRaman.m
+++ RamanAnalysis/GeneralRaman.m
@@ -1,8 +1,11 @@
 clc;
 clear;
+addpath('C:\Users\cborja\OneDrive - Universiteit Antwerpen\SWCNTs\');
+%addpath('C:\Users\Cristian Borja\OneDrive - Universiteit Antwerpen\SWCNTs\');
+import UsefulFunctions.*;
 
-rootpath = 'C:\Users\Cristian Borja\OneDrive - Universiteit Antwerpen\Measurements\Raman\';
-%rootpath = 'C:\Users\cborja\OneDrive - Universiteit Antwerpen\Measurements\Raman\';
+%rootpath = 'C:\Users\Cristian Borja\OneDrive - Universiteit Antwerpen\Measurements\Raman\';
+rootpath = 'C:\Users\cborja\OneDrive - Universiteit Antwerpen\Measurements\Raman\';
 
 %All paths as default
 path_20240111 = [rootpath,'20240111\'];
@@ -18,8 +21,10 @@ paths = {
     path_20240325
     };
 
-%Read and structure data from the paths
-ReadFromPaths(paths);
+
+
+
+ReadRamanFromPaths(paths);
 
 
 %%%--------LABELING--------%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -27,7 +32,6 @@ ReadFromPaths(paths);
 DATA_20240111.FLATHD.N='FlatField 514.5nm';
 DATA_20240111.LL514.N='LaserLine 514.5nm';
 DATA_20240111.LL514HD.N='LaserLine 514.5nm';
-
 
 DATA_20240111.S240111A.N='SF D2O@SWCNT (514.5 nm)';
 DATA_20240111.S240111C.N='SF Methanol@SWCNT (514.5 nm)';
@@ -96,59 +100,59 @@ DATA_20240325.S7H680R.N='CB Dodecane@SWCNT Dial. DGU C (Filled) (680.02 nm)';
 %%%--------MANUAL CORRECTIONS--------%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 %FlatFields Normalization
-DATA_20240111.FLATHD = NormFlatField(DATA_20240111.FLATHD);
-DATA_20240320.FFH520R = NormFlatField(DATA_20240320.FFH520R);
-DATA_20240321.FFH520G = NormFlatField(DATA_20240321.FFH520G);
-DATA_20240321.FFL520G = NormFlatField(DATA_20240321.FFL520G);
-DATA_20240325.FH680R = NormFlatField(DATA_20240325.FH680R);
 
 
 %FF Correction to RBMs region
-DATA_20240111.S240111J.Y = DATA_20240111.S240111J.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111K.Y = DATA_20240111.S240111K.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111KK.Y = DATA_20240111.S240111KK.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111L.Y = DATA_20240111.S240111L.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111M.Y =DATA_20240111.S240111M.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111N.Y =DATA_20240111.S240111N.Y ./ DATA_20240111.FLATHD.Y; 
-DATA_20240111.S240111O.Y =DATA_20240111.S240111O.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111P.Y =DATA_20240111.S240111P.Y ./ DATA_20240111.FLATHD.Y; 
-DATA_20240111.S240111Q.Y =DATA_20240111.S240111Q.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111R.Y =DATA_20240111.S240111R.Y ./ DATA_20240111.FLATHD.Y;
-DATA_20240111.S240111S.Y =DATA_20240111.S240111S.Y ./ DATA_20240111.FLATHD.Y;
+SList = {
+        DATA_20240111.S240111J,
+        DATA_20240111.S240111K,
+        DATA_20240111.S240111KK,
+        DATA_20240111.S240111L,
+        DATA_20240111.S240111M,
+        DATA_20240111.S240111N,
+        DATA_20240111.S240111O,
+        DATA_20240111.S240111P, 
+        DATA_20240111.S240111Q,
+        DATA_20240111.S240111R,
+        DATA_20240111.S240111S
+        };
+
+SList = FlatFieldCorrection(SList, DATA_20240111.FLATHD)
+
 
 %FF Correction to RBMs region
-DATA_20240320.S2H520R.Y = DATA_20240320.S2H520R.Y ./ DATA_20240320.FFH520R.Y;
-DATA_20240320.S3H520R.Y = DATA_20240320.S3H520R.Y ./ DATA_20240320.FFH520R.Y;
-DATA_20240320.S4H520R.Y = DATA_20240320.S4H520R.Y ./ DATA_20240320.FFH520R.Y;
-DATA_20240320.S5H520R.Y = DATA_20240320.S5H520R.Y ./ DATA_20240320.FFH520R.Y;
-DATA_20240320.S6H520R.Y = DATA_20240320.S6H520R.Y ./ DATA_20240320.FFH520R.Y;
-DATA_20240320.S7H520R.Y = DATA_20240320.S7H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S2H520R.Y = DATA_20240320.S2H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S3H520R.Y = DATA_20240320.S3H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S4H520R.Y = DATA_20240320.S4H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S5H520R.Y = DATA_20240320.S5H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S6H520R.Y = DATA_20240320.S6H520R.Y ./ DATA_20240320.FFH520R.Y;
+%DATA_20240320.S7H520R.Y = DATA_20240320.S7H520R.Y ./ DATA_20240320.FFH520R.Y;
 
 %FF Correction to GBand region in LD Mode
-DATA_20240321.S2L520G.Y = DATA_20240321.S2L520G.Y ./ DATA_20240321.FFL520G.Y;
-DATA_20240321.S3L520G.Y = DATA_20240321.S3L520G.Y ./ DATA_20240321.FFL520G.Y;
-DATA_20240321.S4L520G.Y = DATA_20240321.S4L520G.Y ./ DATA_20240321.FFL520G.Y;
-DATA_20240321.S5L520G.Y = DATA_20240321.S5L520G.Y ./ DATA_20240321.FFL520G.Y;
-DATA_20240321.S6L520G.Y = DATA_20240321.S6L520G.Y ./ DATA_20240321.FFL520G.Y;
-DATA_20240321.S7L520G.Y = DATA_20240321.S7L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S2L520G.Y = DATA_20240321.S2L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S3L520G.Y = DATA_20240321.S3L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S4L520G.Y = DATA_20240321.S4L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S5L520G.Y = DATA_20240321.S5L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S6L520G.Y = DATA_20240321.S6L520G.Y ./ DATA_20240321.FFL520G.Y;
+%DATA_20240321.S7L520G.Y = DATA_20240321.S7L520G.Y ./ DATA_20240321.FFL520G.Y;
 
 %FF Correction to GBand region in HD Mode
-DATA_20240321.S2H520G.Y = DATA_20240321.S2H520G.Y ./ DATA_20240321.FFH520G.Y;
-DATA_20240321.S3H520G.Y = DATA_20240321.S3H520G.Y ./ DATA_20240321.FFH520G.Y;
-DATA_20240321.S4H520G.Y = DATA_20240321.S4H520G.Y ./ DATA_20240321.FFH520G.Y;
-DATA_20240321.S5H520G.Y = DATA_20240321.S5H520G.Y ./ DATA_20240321.FFH520G.Y;
-DATA_20240321.S6H520G.Y = DATA_20240321.S6H520G.Y ./ DATA_20240321.FFH520G.Y;
-DATA_20240321.S7H520G.Y = DATA_20240321.S7H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S2H520G.Y = DATA_20240321.S2H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S3H520G.Y = DATA_20240321.S3H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S4H520G.Y = DATA_20240321.S4H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S5H520G.Y = DATA_20240321.S5H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S6H520G.Y = DATA_20240321.S6H520G.Y ./ DATA_20240321.FFH520G.Y;
+%DATA_20240321.S7H520G.Y = DATA_20240321.S7H520G.Y ./ DATA_20240321.FFH520G.Y;
 
 %FF Correction to GBand region in HD Mode
-DATA_20240325.REH680R.Y = DATA_20240325.REH680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.RWH680R.Y = DATA_20240325.RWH680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S2H680R.Y = DATA_20240325.S2H680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S3H680R.Y = DATA_20240325.S3H680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S4H680R.Y = DATA_20240325.S4H680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S5H680R.Y = DATA_20240325.S5H680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S6H680R.Y = DATA_20240325.S6H680R.Y ./ DATA_20240325.FH680R.Y;
-DATA_20240325.S7H680R.Y = DATA_20240325.S7H680R.Y ./ DATA_20240325.FH680R.Y;
+%%DATA_20240325.REH680R.Y = DATA_20240325.REH680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.RWH680R.Y = DATA_20240325.RWH680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S2H680R.Y = DATA_20240325.S2H680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S3H680R.Y = DATA_20240325.S3H680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S4H680R.Y = DATA_20240325.S4H680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S5H680R.Y = DATA_20240325.S5H680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S6H680R.Y = DATA_20240325.S6H680R.Y ./ DATA_20240325.FH680R.Y;
+%DATA_20240325.S7H680R.Y = DATA_20240325.S7H680R.Y ./ DATA_20240325.FH680R.Y;
 
 
 
@@ -175,8 +179,6 @@ GDBand520 = {
         DATA_20240321.S5L520G,
         DATA_20240321.S6L520G,
         DATA_20240321.S7L520G,
-                
-                
         };
     
 GBand520 = {
@@ -187,7 +189,6 @@ GBand520 = {
         DATA_20240321.S6H520G,
         DATA_20240321.S7H520G,
         };
-
 
     
 RBM514 = {
@@ -238,175 +239,85 @@ RBM680 = {
 LG = 1560;
 HG = 1570;
 
-GDBand520 = SubstractBG(GDBand520, 1400, 1500)
-GDBand520 = Normalize(GDBand520,LG, HG);
+GDBand520 = SubstractLinearBG(GDBand520, 1400, 1500);
+GDBand520 = NormalizeSample(GDBand520,LG, HG);
 
-GBand520 = SubstractBG(GBand520, 1540, 1620)
-GBand520 = Normalize(GBand520,LG, HG);
+GBand520 = SubstractLinearBG(GBand520, 1540, 1620);
+GBand520 = NormalizeSample(GBand520,LG, HG);
 
-GDBand514 = SubstractBG(GDBand514,1400, 1500)
-GDBand514 = Normalize(GDBand514,LG,HG)
+GDBand514 = SubstractLinearBG(GDBand514,1400, 1500);
+GDBand514 = NormalizeSample(GDBand514,LG,HG);
 
 
 LR = 150;
 HR = 160;
 
-RBM514 = SubstractBG(RBM514, 130, 210)
-RBM514 = Normalize(RBM514,LR, HR);
+RBM514 = SubstractLinearBG(RBM514, 130, 210);
+RBM514 = NormalizeSample(RBM514,LR, HR);
 
-RBM520 = SubstractBG(RBM520, 130, 210)
-RBM520 = Normalize(RBM520,LR, HR);
+RBM520 = SubstractLinearBG(RBM520, 130, 210);
+RBM520 = NormalizeSample(RBM520,LR, HR);
 
-RBM680 = SubstractBG(RBM680,150, 198)
-RBM680 = Normalize(RBM680,LR, HR)
-
-
+RBM680 = SubstractLinearBG(RBM680,147, 200);
+RBM680 = NormalizeSample(RBM680,LR, HR);
 
 
-%plotSampleList(RBM514, 1)
-%plotSampleList(RBM520, 1)
-%plotSampleList(RBM680, 1)
-%plotSampleList(GDBand514, 1)
-%plotSampleList(GDBand520, 1)
-%plotSampleList(GBand520, 1)
 
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function dataStructures = ReadFromPaths(paths)
-    dataStructures = struct();  
-    for p = 1:length(paths)
-        % Extract the suffix from the path
-        try
-            % Extract the suffix from the path variable
-            dataset_name = strsplit(paths{p}, "\");
-            fieldName = ['DATA_', strrep(dataset_name{7}, '.', '')];
-           
-            dirInfo = dir(fullfile(paths{p}, '*.m3d'));
-            fileList = {dirInfo(~[dirInfo.isdir]).name};
-                        
-            structure = struct();
+%plotRaman(RBM680,3)
+%plotRaman(RBM520, 3)
 
-            % Read the data from the current path
-            for f = 1:length(fileList)
-                raw_spectrum = RdExp([paths{p},fileList{f}]);
-                raw_spectrum(:,2)=[];
-                NumSpec=length(raw_spectrum(1,:))-1;
-                NumDel=1;
-                        X=raw_spectrum(:,1);
-                for i=1:1024
-                    spectrum=sort(raw_spectrum(i,2:NumSpec+1));
-                    Y(:,i)= mean(spectrum(NumDel+1:NumSpec-NumDel));  
-                end
-                sampleName = upper(strrep(fileList{f}, '.m3d',''));
-               
-                structure.(sampleName).X = X;
-                structure.(sampleName).Y = Y;
-                structure.(sampleName).N = sampleName;
-            end 
-           dataStructures.(fieldName) = structure;
-           assignin('caller', fieldName, structure); % Assign data to a variable in the caller workspace
+%plotRaman(RBM514, 1)
+%plotRaman(RBM520, 1)
+%plotRaman(RBM680, 1)
 
-        catch ME
-            % Print the path that caused the error
-            disp(['Error reading data from path: ' paths{p}]);
-            % Re-throw the error
-            rethrow(ME);
-        end
-    end       
-end
+%plotRaman(GDBand514, 0)
+%plotRaman(SList, 0.1);
+SList = SubstractLinearBG(SList,130, 210);
+SList = NormalizeSample(SList,LR, HR);
+plotRaman(SList, 0.0);
 
-function integralValue = computeIntegral(sample, lowerLimit, upperLimit)
-    % Check if the sampleName is in DATA
-    % Extract X and Y values for the specified sample
-    x = sample.X;
-    y = sample.Y;
-    % Define the function to integrate
-    f = @(xi) interp1(x, y, xi, 'pchip');
+%plotRaman(GBand520, 0)
 
-    % Calculate the integral
-    integralValue = integral(f, lowerLimit, upperLimit);
-end
+GDBand520 = GDBandPeaksCalculation(GDBand520, 1580,1600,1560,1570,1300,1400);
+exportGDBandPeaks(GDBand520, 'dsfsd.csv');
 
-function maximumValue = computeMaximum(sample, lowerLimit, upperLimit)
-    % Check if the sampleName is in DATA
-    % Extract X and Y values for the specified sample
-    x = sample.X;
-    y = sample.Y;
-    % Define the function to integrate
+
+
+%%%%%TODO
+%Use an excel file to handle the labeling
+
+function labeledData = assignLabelsFromExcel(data, excelFile)
+    % Read the Excel file
+    excelData = readtable(excelFile);
     
-    indicesInRange = find(x >= lowerLimit & x <= upperLimit);
-    yInRange = y(indicesInRange);
-
-    maximumValue = max(yInRange);
-end
-
-function Normed = NormFlatField(Flat)
-        Flat.Y = Flat.Y / computeIntegral(Flat,Flat.X(end), Flat.X(1));
-        Normed = Flat;
-end
-
-function NormedSamples = Normalize(samplesToNormalize, lowerLimit, upperLimit)
-    NormedSamples = cell(size(samplesToNormalize));
-    % Iterate over each sample to be normalized
-    for sampleIdx = 1:length(samplesToNormalize)
-        currentSample = samplesToNormalize{sampleIdx};
-        %currentSample.Y = currentSample.Y/computeIntegral(currentSample,lowerLimit, upperLimit);
-        currentSample.Y = currentSample.Y/computeMaximum(currentSample,lowerLimit, upperLimit);
-        NormedSamples{sampleIdx} = currentSample;
-    end
-end
-
-function CorrectedSpectra = SubstractBG(samplesToCorrect, X1, X2)
-    CorrectedSpectra = cell(size(samplesToCorrect));
-    for i = 1:length(samplesToCorrect)
-        currentSample = samplesToCorrect{i};
-        X = currentSample.X;
-        Y = currentSample.Y;
-        % Find the indices of X1 and X2 in the X array
-        [~, idx_range1] = min(abs(X - X1));
-        [~, idx_range2] = min(abs(X - X2));
+    % Extract Raman and N columns
+    ramanColumn = excelData.Raman;
+    nColumn = excelData.N;
+    
+    % Initialize labeled data structure
+    labeledData = struct();
+    
+    % Loop through each variable in the input data
+    variableNames = fieldnames(data);
+    for i = 1:numel(variableNames)
+        variableName = variableNames{i};
         
-        % Calculate the slope m
-        m = (Y(idx_range2) - Y(idx_range1)) / (X2 - X1);
-        % Calculate the y-intercept b
-        b = Y(idx_range1) - m*X1;
-
-        % Calculate the linear background using the equation of the line
-        background = m*X + b;
-       
-        % Subtract the background
-        currentSample.Y = currentSample.Y - background';
-        CorrectedSpectra{i} = currentSample;
+        % Find the corresponding N value in the Excel data
+        index = strcmp(variableName, ramanColumn);
+        if any(index)
+            nValue = nColumn(index);
+            
+            % Assign N value to the labeled data structure
+            labeledData.(variableName) = data.(variableName);
+            labeledData.(variableName).N = nValue;
+        else
+            % If the variable name is not found in the Excel data, display a warning
+            warning(['Variable "', variableName, '" not found in Excel data.']);
+        end
     end
 end
-
-function plotSampleList(SamplesToPlot, offset)
-    % Create a figure for the plot
-    figure;
-    % Iterate over each sample
-    for sampleIdx = 1:length(SamplesToPlot)
-        currentSample = SamplesToPlot{sampleIdx};
-            % Get the current sample, X values, and Y values
-            currentX = currentSample.X;
-            currentY = currentSample.Y - offset*sampleIdx;
-            currentN = currentSample.N;
-            plot(currentX, currentY, 'DisplayName', currentN,'LineWidth', 1.3);
-            hold on; % Add spectra to the same plot
-    end
-
-    % Add labels and legend
-    xlabel('Raman Shift (cm^{-1})');
-    ylabel('Intesity (a.u.)');
-    title('Raman Spectra');
-    legend('show');
-    % Optional: Customize the plot further if needed
-    grid on;
-    % Hold off to stop adding new plots to the current figure
-    hold off;
-    
-end
-
 
 
 
