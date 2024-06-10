adf1fee5cbdf76c67f33c2afa9215cb7d7a48715
Cristian Borja <cmbp98@gmail.com>
2024-06-10 19:57:14 +0200

    2024 06 10

---
+++ RamanAnalysis/KIT_Samples/Analysis_FilledSamples.m
@@ -0,0 +1,79 @@
+clc;
+clear;
+addpath('C:\Users\cborja\OneDrive - Universiteit Antwerpen\SWCNTs\');
+%addpath('C:\Users\Cristian Borja\OneDrive - Universiteit Antwerpen\SWCNTs\');
+import UsefulFunctions.*;
+
+%rootpath = 'C:\Users\Cristian Borja\OneDrive - Universiteit Antwerpen\Measurements Data\Raman\';
+rootpath = 'C:\Users\cborja\OneDrive - Universiteit Antwerpen\Measurements Data\Raman\';
+
+%All paths as default
+path_20240610 = [rootpath,'20240610\'];
+
+%Select the paths of interest
+
+paths = {
+    path_20240610
+    };
+
+
+ReadRamanFromPaths(paths);
+
+
+%%%--------LABELING--------%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+DATA_20240610.EAH514R.N='Pending';
+DATA_20240610.EAL514GD.N='Pending';
+DATA_20240610.FFH514R.N='Pending';
+DATA_20240610.LL514A.N='Laser';
+DATA_20240610.LL514B.N='Laser';
+DATA_20240610.S2H514R.N='Pending';
+DATA_20240610.S2L514GD.N='Pending';
+DATA_20240610.S3H514R.N='Pending';
+DATA_20240610.S3L514GD.N='Pending';
+DATA_20240610.S4H514R.N='Pending';
+DATA_20240610.S4L514GD.N='Pending';
+DATA_20240610.S5H514R.N='Pending';
+DATA_20240610.S5L514GD.N='Pending';
+DATA_20240610.S6H514R.N='Pending';
+DATA_20240610.S6L514GD.N='Pending';
+DATA_20240610.S7H514R.N='Pending';
+DATA_20240610.S7L514GD.N='Pending';
+DATA_20240610.WAH514R.N='Pending';
+DATA_20240610.WAL514GD.N='Pending';
+
+%%%--------MANUAL CORRECTIONS--------%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+%%%--------SAMPLE COMPARISION--------%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+Samples = {
+% DATA_20240610.EAH514R
+DATA_20240610.EAL514GD
+% DATA_20240610.S2H514R
+DATA_20240610.S2L514GD
+% DATA_20240610.S3H514R
+DATA_20240610.S3L514GD
+% DATA_20240610.S4H514R
+DATA_20240610.S4L514GD
+% DATA_20240610.S5H514R
+DATA_20240610.S5L514GD
+% DATA_20240610.S6H514R
+DATA_20240610.S6L514GD
+% DATA_20240610.S7H514R
+DATA_20240610.S7L514GD
+% DATA_20240610.WAH514R
+DATA_20240610.WAL514GD    
+};
+
+
+%for RBMs
+
+% Samples = FlatFieldCorrection(Samples, DATA_20240610.FFH514R)
+% Samples = UsefulFunctions.SubstractLinearBG(Samples, 137, 200)
+% Samples = UsefulFunctions.NormalizeSample(Samples,140, 160)
+
+%for G/D Bands
+Samples = UsefulFunctions.SubstractLinearBG(Samples, 1250, 1650)
+Samples = UsefulFunctions.NormalizeSample(Samples,1585, 1595)
+
+plotRaman(Samples, 0.0)
